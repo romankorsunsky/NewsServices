6419bc0cbd7339c0cec2a17fcd934e6166196a0a
RomanKorsunsky <korsunsky.roma@gmail.com>
2025-10-26 18:10:03 +0200

    git is very intuitive(not)

--- authserv/src/main/java/none/romank/authserv/BadUserRegistrationException.java
+++ authserv/src/main/java/none/romank/authserv/BadUserRegistrationException.java
@@ -2,6 +2,6 @@ package none.romank.authserv;
 
 public class BadUserRegistrationException extends RuntimeException{
     public BadUserRegistrationException(String msg){
-        super();
+        super(msg);
     }
 }
